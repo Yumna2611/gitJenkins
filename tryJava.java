15f41180d0cdb3ccd7f618f739f59a6c57f49cb4
Yumna Khan <78953451+Yumna2611@users.noreply.github.com>
2022-03-25 11:24:28 +0530

    Update tryJava.java

--- tryJava.java
+++ tryJava.java
@@ -3,7 +3,7 @@ public class tryJava
 public static void main(String args[])  
 {  
 //character array  
-char[] ch = {'w', 'e', 'l', 'c', 'o', 'm', 'e', ' ' , 't', 'o', ' ', 'J', 'a', 'v', 'a', 't', 'p', 'o', 'i', 'n', 't'};  
+char[] ch = {'w', 'e', 'l', 'c', 'o', 'm', 'e', ' ' , 't', 'o', ' ', 'J', 'e', 'n', 'k', 'i', 'n', 's'};  
 //constructor of the String class that parses char array as a parameter  
 String string = new String(ch);  
 //prints the string  
